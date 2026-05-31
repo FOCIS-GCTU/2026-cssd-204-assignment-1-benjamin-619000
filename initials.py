cac07024610fc9df5416ecdaf393c9fb84159a47
benjamin-619000 <2425401956@live.gctu.edu.gh>
2026-05-31 14:01:06 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,36 @@
+# File name: initials.py
+# Description: A print out of my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Benjamin Kweku Afful
+# STUDENT ID: 2425401956
+# Email: 2425401956@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Benjamin Kweku Afful, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
 
 def main():
-  pass #code goes here
+    # Prints my initials in large stylized letters
 
+    print()
+    print("...BKA")
+    print()
+
+    print("...BBBBBBBBBBBB........KKK......KKK........AAAAAAAAAA.....")
+    print("...BBB......BBB........KKK....KKK..........AAA....AAA.....")
+    print("...BBB......BBB........KKK..KKK............AAA....AAA.....")
+    print("...BBBBBBBBBBBB........KKKKKK..............AAAAAAAAAA.....")
+    print("...BBB......BBB........KKKKKK..............AAA....AAA.....")
+    print("...BBB......BBB........KKK..KKK............AAA....AAA.....")
+    print("...BBB......BBB........KKK....KKK..........AAA....AAA.....")
+    print("...BBBBBBBBBBBB........KKK......KKK........AAA....AAA.....")
+    print("...BBB......BBB...**...KKK......KKK...**...AAA....AAA...**")
+    print("...BBBBBBBBBBBB...**...KKK......KKK...**...AAA....AAA...**")
+
+    print()
 
 
 main()
